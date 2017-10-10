9fd3419eb1e458625fe34a7320efef0afff2377d
vagrant <vagrant@precise32.(none)>
2017-10-10 00:39:03 +0000

    added new and create

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,4 +5,27 @@ class PortfoliosController < ApplicationController
   end
 
 
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio successfully created' }
+        
+      else
+        format.html { render :new }
+        
+      end
+    end
+  end
+
+
+
+
+
+
 end
