0faa5e98a122d94921964d32354258afad349f23
vagrant <vagrant@precise32.(none)>
2017-10-10 05:42:16 +0000

    edit and update

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,6 +9,10 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
 
+  def edit
+  	@portfolio_item = Portfolio.find(params[:id])
+  end
+
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -22,6 +26,20 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+   def update
+   	@portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+        
+      else
+        format.html { render :edit }
+        
+      end
+    end
+  end
+
 
 
 
