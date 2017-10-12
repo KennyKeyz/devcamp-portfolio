634c7019ba94ce239ae183c4a51d81682dcadbf0
vagrant <vagrant@precise32.(none)>
2017-10-12 13:23:18 +0000

    integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,4 +68,14 @@ puts "9 portfolio items created"
   )
 end
 
-puts "1 portfolio items created"
+puts "1 portfolio item created"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+ 
+  name: "Technology #{technology}"
+  )
+    
+end  
+
+puts "3 Technologies created"
