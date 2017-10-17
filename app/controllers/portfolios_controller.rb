930e0f783347669b458ab92a1fdcd37d15dbcd58
vagrant <vagrant@precise32.(none)>
2017-10-17 15:08:10 +0000

    portfolio before action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout "portfolio"
 
   def index
@@ -16,11 +17,11 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-  	@portfolio_item = Portfolio.find(params[:id])
+  	
   end
 
   def edit
-  	@portfolio_item = Portfolio.find(params[:id])
+  	
     3.times{ @portfolio_item.technologies.build }
   end
 
@@ -39,7 +40,7 @@ class PortfoliosController < ApplicationController
   end
 
    def update
-   	@portfolio_item = Portfolio.find(params[:id])
+   	
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
@@ -52,7 +53,7 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-  	@portfolio_item = Portfolio.find(params[:id])
+  	
     @portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
@@ -70,5 +71,9 @@ class PortfoliosController < ApplicationController
     
   
   end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end  
 
 end
